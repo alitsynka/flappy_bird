aacd696470a95d6f84d7760c9eca7d10028b2145
alitsynka <alina.strok@icloud.com>
2024-05-15 17:18:16 +0300

    last updt

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import {useEffect, useState} from "react";
  * All the constant values required for the game to work.
  * By changing these values we can effect the working of the game.
  */
-const BIRD_HEIGHT = 38;
-const BIRD_WIDTH = 43;
-const WALL_HEIGHT = 600;
+const BIRD_HEIGHT = 33;
+const BIRD_WIDTH = 36;
+const BIRD_POSITION = 95;
+const WALL_HEIGHT = 700;
 const WALL_WIDTH = 400;
-const GRAVITY = 5.5;
-const OBJ_WIDTH = 56;
+const GRAVITY = 7;
+const OBJ_WIDTH = 110;
 const OBJ_SPEED = 6;
 const OBJ_GAP = 200;
+const BIRD_SPEED = 28;
+const WALL_SPEED = BIRD_SPEED;
+
+let currentSpeed = -5;
+
+let upSteps = 0;
 
 /**
  * This function is the main component which renders all the game objects.
@@ -29,21 +36,38 @@ function App() {
     const [modal, setModal] = useState('flex');
     const [gameOver, setGameOver] = useState(-150);
     const [positionCurtain, setpositionCurtain] = useState(0);
+    const [positionCutBird, setpositionCutBird] = useState(-1);
+    const [positionRunBird, setpositionRunBird] = useState(-100);
     const [displayCloud, setdisplayCloud] = useState('flex');
 
 
     //End the game when the player hits the bottom of the screen.
     useEffect(() => {
         let intVal;
-        let intVal2;
         if (isStart && birdpos < WALL_HEIGHT - BIRD_HEIGHT) {
+
+
             intVal = setInterval(() => {
-                setBirdpos((birdpos) => birdpos + GRAVITY);
-            }, 18);
+
+                setBirdpos(birdpos + currentSpeed);
+                currentSpeed += 2
+                // if (upSteps) {
+                //     setBirdpos(birdpos => birdpos - 10)
+                //     upSteps--
+                // } else {
+                //     setBirdpos((birdpos) => birdpos + GRAVITY);
+                //
+                // }
+            }, BIRD_SPEED);
+
+
         } else {
+
             if (isStart) {
                 setGameOver(50)
-                setBirdpos(3000);
+                setBirdpos(300);
+                setpositionCutBird(-1)
+                setpositionRunBird(10)
             }
             // intVal2 = setInterval(() => {
             //     console.log('ksklsd')
@@ -55,70 +79,95 @@ function App() {
         }
         return () => {
             clearInterval(intVal);
-            clearInterval(intVal2);
         }
     });
 
+    const customRandom = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
     //Generating the pipes(obstacles) for the game.
     useEffect(() => {
         let objval;
         if (isStart && objPos >= -OBJ_WIDTH) {
             objval = setInterval(() => {
                 setObjPos((objPos) => objPos - OBJ_SPEED);
-            }, 24);
+            }, WALL_SPEED);
 
             return () => {
                 clearInterval(objval);
             };
         } else {
             setObjPos(WALL_WIDTH);
-            setObjHeight(Math.floor(Math.random() * (WALL_HEIGHT - OBJ_GAP)));
+            setObjHeight(Math.floor(customRandom(10, 90) * 0.01 * (WALL_HEIGHT - OBJ_GAP)));
             if (isStart) setScore((score) => score + 1);
         }
     }, [isStart, objPos]);
 
-    // const [time, setTime] = useState(700)
+    const [time, setTime] = useState(7)
 
     //Ends the game of the player hits one of the obstacles.
     useEffect(() => {
         let topObj = birdpos >= 0 && birdpos < objHeight;
+
         let bottomObj =
             birdpos <= WALL_HEIGHT &&
-            birdpos >=
-            WALL_HEIGHT - (WALL_HEIGHT - OBJ_GAP - objHeight) - BIRD_HEIGHT;
+            birdpos >= OBJ_GAP + objHeight - BIRD_HEIGHT;
+
         let intVal2;
+
+        console.log('--------------------------')
+        console.log(topObj)
+        console.log(bottomObj)
+        console.log(birdpos)
+        console.log(objHeight)
+
         if (
-            objPos >= OBJ_WIDTH &&
-            objPos <= OBJ_WIDTH + 80 &&
+            objPos >= BIRD_POSITION - OBJ_WIDTH + 7 &&
+            objPos <= BIRD_POSITION + BIRD_WIDTH - 7 &&
             (topObj || bottomObj)
         ) {
 
+
             if (isStart) {
+                // debugger
+                console.log('skdjfksdjfjsdfksd')
                 setGameOver(50)
-                setBirdpos(3000);
+                setpositionCutBird(-1)
+                setpositionRunBird(10)
+                // setBirdpos(300);
                 setIsStart(false);
             }
             intVal2 = setInterval(() => {
                 console.log('popali v useEffect')
-
-                setBirdpos(300);
+                // setBirdpos(300);
                 setGameOver(-150)
                 setModal('flex')
             }, 7000);
 
+        } else {
+
+            // console.log(objPos);
+            // console.log(topObj)
+            // console.log(bottomObj)
+            // console.log(objHeight)
+            // console.log(birdpos)
+            // console.log(WALL_HEIGHT - (WALL_HEIGHT - OBJ_GAP - objHeight) - BIRD_HEIGHT)
         }
         return () => {
             clearInterval(intVal2);
+            // clearTimeout(intVal3);
         }
-    }, [isStart, birdpos, objHeight, objPos, modal]);
+    }, [isStart, birdpos, objHeight, objPos]);
 
     useEffect(() => {
         const handleKeyPress = (e) => {
             if (e.code === 'Space') {
-                setIsStart(true);
-                setModal('none')
-                setGameOver(-150)
-                setBirdpos((prev) => prev - 100);
+                handler()
+                // setIsStart(true);
+                // setModal('none')
+                // setGameOver(-150)
+                //
+                // setBirdpos((prev) => prev - 100);
             }
         };
 
@@ -130,44 +179,60 @@ function App() {
     }, [isStart, birdpos]); // Add isStart and birdpos to the dependency list
 
     //useEffect with dependency Time
-    // useEffect(() => {
-    //     let timer;
-    //
-    //     if (gameOver === 50) {
-    //         timer = setInterval(() => {
-    //             console.log('popali v useEffect')
-    //             if (time !== 0) {
-    //                 setTime(time - 1)
-    //                 return
-    //             } else if (time === 0) {
-    //                 setBirdpos(300);
-    //                 setGameOver(-150)
-    //                 setModal('flex')
-    //                 setTime(7000)
-    //             }
-    //
-    //
-    //         }, 1000);
-    //     }
-    //     return () => {
-    //         clearInterval(timer);
-    //     }
-    // }, [gameOver, time])
+    useEffect(() => {
+        let timer;
+
+        if (gameOver === 50) {
+            timer = setInterval(() => {
+                console.log('popali v useEffect')
+                if (time !== 0) {
+                    setTime(time - 1)
+                    return
+                } else if (time === 0) {
+                    setpositionCurtain(0)
+                    setdisplayCloud("flex")
+                    setGameOver(-150)
+                    setpositionCutBird(-1)
+                    setpositionRunBird(-100)
+                    setModal('flex')
+                    setTime(7)
+                }
+
+
+            }, 1000);
+        }
+        return () => {
+            clearInterval(timer);
+        }
+    }, [gameOver, time])
 
 
     //Handles the player movements.
     const handler = () => {
+        console.log(birdpos)
         if (!isStart) {
             setIsStart(true);
             setModal('none')
             setGameOver(-150)
             setpositionCurtain(-100)
+            setpositionCutBird(-100)
+            setpositionRunBird(-100)
             setdisplayCloud("none")
-            // setModal('flex')
             setBirdpos(300);
             setScore(0);
-        } else if (birdpos < BIRD_HEIGHT) setBirdpos(0);
-        else setBirdpos((birdpos) => birdpos - 105);
+            currentSpeed = -5
+        } else if (birdpos < BIRD_HEIGHT) {
+            console.log('0000')
+            setBirdpos(0);
+        } else {
+            // upSteps = 5
+            currentSpeed = -15
+            // setBirdpos((birdpos) => birdpos - 40);
+            // setTimeout(() => {
+            //     setBirdpos((birdpos) => birdpos - 50);
+            // }, BIRD_SPEED)
+
+        }
     };
 
     const handleKeyDown = (event) => {
@@ -183,7 +248,10 @@ function App() {
 
     return (
         //Whole body of the game.
-        <Home onClick={handler} onKeyDown={handleKeyDown} tabIndex="0">
+        <Home tabIndex="0">
+            {
+                isStart && <ClickBackground onClick={handler} onKeyDown={handleKeyDown}></ClickBackground>
+            }
             <Background height={WALL_HEIGHT} width={WALL_WIDTH}>
                 {
                     isStart ? <ScoreShow>Очки: {score}</ScoreShow> : ''
@@ -192,7 +260,8 @@ function App() {
                 {/*    <h3>Chizhik Game</h3>*/}
                 {/*</Modal>*/}
                 <Curtain bottom={positionCurtain}>
-                    <SubscribeLink onClick={handler} style={{top: "-17%", maxWidth: "240px"}} display={"block"}>
+                    <SubscribeLink onClick={handler} style={{top: "-17%", maxWidth: "240px"}}
+                                   display={"block"}>
                         Играть
                     </SubscribeLink>
                 </Curtain>
@@ -200,6 +269,7 @@ function App() {
                     <p>Сыграй</p>
                     <p>полетай!</p>
                 </Cloud>
+
                 {
                     gameOver
                     && <Modal top={gameOver}>
@@ -212,8 +282,14 @@ function App() {
                                        display={"none"}
                                        target={"_blank"} onClick={(e) => {
                             e.stopPropagation()
-                            setBirdpos(300);
+                            // setBirdpos(300);
+                            // setGameOver(-150)
+                            // setModal('flex')
+                            setpositionCurtain(0)
+                            setdisplayCloud("flex")
                             setGameOver(-150)
+                            setpositionCutBird(-1)
+                            setpositionRunBird(-100)
                             setModal('flex')
                         }}>
                             Подписаться
@@ -221,9 +297,11 @@ function App() {
                         <SubscribeLink onClick={handler} display={"none"}>
                             Играть
                         </SubscribeLink>
+
                     </Modal>
                 }
-                {(!isStart && birdpos === 300) ? <Startboard>Играть</Startboard> : null}
+                <CutBird bottom={positionCutBird}/>
+                <RunBird right={positionRunBird}/>
                 <Obj
                     height={objHeight}
                     width={OBJ_WIDTH}
@@ -235,13 +313,13 @@ function App() {
                     height={BIRD_HEIGHT}
                     width={BIRD_WIDTH}
                     top={birdpos}
-                    left={95}
+                    left={BIRD_POSITION}
                 />
                 <Obj
                     height={WALL_HEIGHT - OBJ_GAP - objHeight}
                     width={OBJ_WIDTH}
                     left={objPos}
-                    top={WALL_HEIGHT - (objHeight + (WALL_HEIGHT - OBJ_GAP - objHeight))}
+                    top={OBJ_GAP}
                     deg={0}
                 />
             </Background>
@@ -261,6 +339,13 @@ const Home = styled.div`
     flexDirection: 'column';
     overflow: hidden;
 `;
+const ClickBackground = styled.div`
+    position: absolute;
+    background: rgba(10, 35, 66, 0);
+    z-index: 99999999;
+    width: 100%;
+    height: 100%;
+`
 
 const Background = styled.div`
     // background-image: url("/flappy_bird/images/background-day.png");
@@ -270,27 +355,28 @@ const Background = styled.div`
     //background: #303f9c;
     width: ${(props) => props.width}px;
     height: ${(props) => props.height}px;
-    border: 2px dashed orchid;
+    //border: 2px dashed orchid;
     position: relative;
     overflow: hidden;
-    //border: 3px solid #ff0059;
 `;
 
 const Bird = styled.div`
     position: absolute;
-    background-image: url("/flappy_bird/images/yellowBird.svg");
+    background-image: url("/flappy_bird/images/BirdWithoutTail.svg");
+    //border: 1px dashed black;
     background-repeat: no-repeat;
-    background-size: ${(props) => props.width}px ${(props) => props.height}px;
-    width: ${(props) => props.width}px;
-    height: ${(props) => props.height}px;
+    background-size: contain;
+    width: 36px;
+    height: 33px;
+    border-radius: 30%;
     top: ${(props) => props.top}px;
     left: ${(props) => props.left}px;
-    transition: all 0.05s ease-in;
+    transition: all ${BIRD_SPEED}ms ease-in;
 `;
 const Modal = styled.div`
     position: absolute;
     top: ${(props) => props.top ?? 50}%;
-    z-index: 999999999;
+    z-index: 99;
     width: 80%;
     padding: 50px 35px 35px 35px;
     background-color: #ffffff;
@@ -306,12 +392,23 @@ const Modal = styled.div`
     left: 50%;
     transform: translate(-50%, -50%);
     transition: all 0.6s ease-in-out;
+
+    &:after {
+        content: '';
+        position: absolute;
+        width: 70px;
+        height: 65px;
+        background: url("/flappy_bird/images/cloudBird.svg") no-repeat;
+        background-size: cover;
+        top: -60px;
+        right: -10px;
+    }
 `;
 
 const Obj = styled.div`
     position: relative;
-    bordee:2px dashed orange;
-    background-image: url("/flappy_bird/images/column4.svg");
+    //border: 1px dashed #0a3e61;
+    background-image: url("/flappy_bird/images/col3.png");
     //background-repeat: repeat-y;
     //background-size: cover;
     //background-position: top;
@@ -340,17 +437,25 @@ const Startboard = styled.div`
 `;
 
 const ScoreShow = styled.div`
+    width: 100%;
+    max-width: 185px;
+    height: 52px;
     position: absolute;
     top: 40px;
     left: 50%;
     transform: translate(-50%, 0%);
-    z-index: 1;
     font-weight: bold;
     font-size: 30px;
+    line-height: 30px;
     color: #000000;
-    //width: 0;
-    //height: 0;
-    //opacity: 0;
+    background: white;
+    border-radius: 19px;
+    z-index: 9999999999999999;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 3px 4px black
 `;
 
 const SubscribeLink = styled.a`
@@ -363,10 +468,11 @@ const SubscribeLink = styled.a`
     line-height: 35px;
     border-radius: 25px;
     padding: 15px 30px;
+    text-align: center;
     position: relative;
     color: black;
     text-decoration: none;
-    text-align: center;
+
 
     &:after {
         content: '';
@@ -450,15 +556,27 @@ const Curtain = styled.div`
     height: 50%;
     bottom: ${props => props.bottom}%;
     transition: all 0.3s ease-in-out;
-
-    &:after {
-        content: "";
-        background: url("/flappy_bird/images/CutBird.svg") no-repeat;
-        background-size: cover;
-        width: 200px;
-        height: 160px;
-        bottom: 0;
-        position: absolute;
-        left: -1px;
-    }
+`
+const CutBird = styled.div`
+    background: url("/flappy_bird/images/CutBird.svg") no-repeat;
+    background-size: cover;
+    width: 200px;
+    height: 160px;
+    bottom: ${props => props.bottom}%;
+    transition: all 0.5s ease-in-out;
+    position: absolute;
+    left: -1px;
+    z-index: 9999999;
+`
+const RunBird = styled.div`
+    background: url("/flappy_bird/images/runBird.svg") no-repeat;
+    background-size: cover;
+    width: 108px;
+    height: 100px;
+    bottom: 30px;
+    transition: all 0.7s ease-in-out;
+    position: absolute;
+    right: ${props => props.right}%;
+    //border:2px dashed purple;
+    z-index: 9999999;
 `
